3896573a1a7a54f9ec32f8d832079dfdb497012d
utkarsh sharma <utkarshsharma366@gmail.com>
2026-02-10 21:50:03 +0530

    practicing Scripting

--- month-1-linux-foundation/day-5-basic-bash-scripting/scripts/practice1.sh
+++ month-1-linux-foundation/day-5-basic-bash-scripting/scripts/practice1.sh
@@ -9,5 +9,12 @@ executed
 comment
 
 name="Utkarsh"
+name="Alpha"
 
-echo "Name is $name"
+echo "Name is $name and date is $date
+echo "enter the name:"
+
+read username
+
+echo "you entered the $username
+
